5b71597afadd78e52e8c2c3837ee77214902973d
Alex139-tech <deepakraj90054@emai.com>
2026-06-24 17:06:11 +0545

    product-admin change

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 from .models import Product
-# Register your models here.
 
-admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    # Columns to display in the admin list view
+    list_display = (
+        'title', 
+        'current_price', 
+        'original_price', 
+        'get_discount_percentage',
+        'is_on_sale', 
+        'created_at'
+    )
+    
+    list_filter = ('is_on_sale', 'created_at')
+    search_fields = ('title', 'description')
+    list_editable = ('current_price', 'original_price', 'is_on_sale')
+    date_hierarchy = 'created_at'
+
+    
+    @admin.display(description='Discount (%)')
+    def get_discount_percentage(self, obj):
+        return f"{obj.discount_percentage}%" if obj.discount_percentage > 0 else "0%"
